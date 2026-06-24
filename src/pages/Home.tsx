f4253aa820e3346491f13bad905e7f9317b4809d
Cameron Mirka <cameronmirka@hotmail.com>
2026-06-23 21:17:14 -0400

    No no no this is the grammar fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -187,7 +187,7 @@ function Home() {
                   maxWidth: 860,
                 }}
               >
-                Wildlife photographs from Central Ohio.
+                Wildlife photographs from beautiful Central Ohio.
               </Typography>
               <Typography
                 variant="body1"
